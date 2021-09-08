710e89d0d1938bddada7a622904eff61130e0804
HassaanAbbasi <hassaan.abbasi@ryerson.ca>
2021-09-07 21:18:21 -0400

    Added submission button, lists, and functionality for links

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-let inputButton = document.getElementById("input-btn")
+const inputButton = document.getElementById("input-btn"); //Blue button
+const input = document.getElementsByClassName("input-el"); //Text Field
+const list = document.getElementById("list"); //List
+let links = []; //Stores the saved links
 
 inputButton.addEventListener("click", function() {
-    
-})
+    var inputVal = input[0].value;
+    if(inputVal != "")
+    {
+        links.push(inputVal);
+        input[0].value = "";
+        listUpdate(links.length - 1);
+    }
+    else
+    {
+        alert("Whoops! You can't save this tab with a blank input. Please write anything in the input line and press 'Save Tab'.");
+    }
+});
+
+function listUpdate(link)
+{
+    const name = links[link];
+    var item = document.createElement("li");
+    var link = document.createElement("a");
+    link.innerText = name;
+    link.setAttribute('href', window.location.href);
+    link.setAttribute('target', "_blank");
+    item.appendChild(link);
+    list.appendChild(item);
+}
