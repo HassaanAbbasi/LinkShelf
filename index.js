d84d53cd03dae114d23b9ce88b766755d6e1eb85
HassaanAbbasi <hassaan.abbasi@ryerson.ca>
2021-09-09 11:49:58 -0400

    Total refactor of index.js
    After consulting a colleague with more experience in JS, I decided to alter the way I wrote my code to follow better practices

--- index.js
+++ index.js
@@ -1,80 +1,101 @@
-const inputButton = document.getElementById("input-btn"); //Blue button
-const deleteButton = document.getElementById("delete-btn") //Red button
-const input = document.getElementsByClassName("input-el"); //Text Field
-const list = document.getElementById("list"); //List
-let links = []; //Stores the saved link names
-const linksFromLocalStorage = JSON.parse(localStorage.getItem("links"));
+let data = {}
+const saveBtn = document.getElementById("save-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const note = document.getElementById("note")
+const list = document.getElementById("list")
 
-if(linksFromLocalStorage != null)
-{
-    links = linksFromLocalStorage;
-    listUpdate();
-};
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
+        return v.toString(16)
+    })
+}
 
-inputButton.addEventListener("click", function() {
-    var inputVal = input[0].value;
-    if(inputVal != "")
-    {
-        links.push(inputVal);
-        input[0].value = "";
-        localStorage.setItem("links", JSON.stringify(links));
-        listUpdate();
-    }
-    else
-    {
-        alert("Whoops! You can't save this tab with a blank input. Please write anything in the input line and press 'Save Tab'.");
-    }
-});
-
-input[0].addEventListener("keyup", function(e) {
-    if(e.keyCode === 13)
-    {
-        e.preventDefault();
-        inputButton.click();
-    }
-})
-
-function listUpdate()
-{
-    list.innerHTML = "";
-    for (let i = 0; i < links.length; i++) 
-    {
-        const name = links[i];
-
-        var item = document.createElement("li");
-        var link = document.createElement("a");
-        link.innerText = name;
-        
-        var btnRemove = document.createElement("INPUT");
-        btnRemove.value = "❌";
-        btnRemove.type = "button";
-        btnRemove.id = "delete-btn-small";
-        btnRemove.onclick = function () {
-            deleteSmallButton(name);
-        };
-        
-        link.setAttribute('href', url);
-        link.setAttribute('target', "_blank");
-
-        item.appendChild(link);
-        item.appendChild(btnRemove);
-        list.appendChild(item);
+let loadData = () => {
+    const localData = localStorage.getItem("data")
+    if (localData) {
+        console.log(`loading: ${localData}`)
+        data = JSON.parse(localData)
     }
 }
 
-deleteButton.addEventListener("click", function() {
-    if(links.length == 0)
-    {
-        alert("There are no links in your list to delete.");
+let saveData = () => {
+    localStorage.setItem("data", JSON.stringify(data))
+}
+
+const render = () => {
+    console.log('rendering')
+    list.innerHTML = ''
+
+    for (const id in data) {
+        const { url, note } = data[id]
+        console.log(`rendering: ${id}, ${url}, ${note}`)
+        
+        const item = document.createElement("li")
+        const link = document.createElement("a")
+        link.innerText = note
+        
+        const btnRemove = document.createElement("INPUT")
+        btnRemove.value = "❌"
+        btnRemove.type = "button"
+        btnRemove.id = "delete-btn-small"
+        btnRemove.onclick = () => {
+            handleDelete(id)
+        }
+        
+        link.setAttribute('href', url)
+        link.setAttribute('target', "_blank")
+
+        item.appendChild(link)
+        item.appendChild(btnRemove)
+        list.appendChild(item)
     }
-    localStorage.clear();
-    links = [];
-    listUpdate();
+}
+
+const handleDelete = (id) => {
+    console.log(`deleteing ${id}`)
+    delete data[id]
+    saveData()
+    render()
+}
+
+const handleSave = (url, note) => {
+    console.log(`saving ${url}, ${note}`)
+    
+    const id = uuidv4()
+    data[id] = { url, note }
+    saveData()
+    render()
+    note.value = ''
+}
+
+saveBtn.addEventListener("click", function() {
+    const value = note.value
+    if (value === "") {
+        alert("Whoops! You can't save this tab with a blank input. Please write anything in the input line and press 'Save Tab'.")
+        return
+    }
+
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        let url = tabs[0].url
+        handleSave(url, value)
+    })
+
+    note.value = ""
 })
 
-function deleteSmallButton(name)
-{
-    links = links.filter(function (val,index,arr) {return val != name});
-    localStorage.setItem("links", JSON.stringify(links));
-    listUpdate();
-}
+note.addEventListener("keyup", function(e) {
+    if(e.keyCode === 13){
+        e.preventDefault()
+        saveBtn.click()
+    }
+})
+
+deleteBtn.addEventListener("click", function(e) {
+    data = {}
+    saveData()
+    render()
+})
+
+loadData()
+render()
